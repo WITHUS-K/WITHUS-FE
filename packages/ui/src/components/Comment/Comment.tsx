a72aa2f02e134e3f33ae8d29b4ba6ac346ef2c33
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-04-23 01:44:46 +0900

    feat: Comment 컴포넌트 (#12)

---
+++ packages/ui/src/components/Comment/Comment.tsx
@@ -0,0 +1,28 @@
+import { Flex } from '../Flex';
+import { Profile } from '../Profile/Profile';
+import { Text } from '../Text';
+import * as styles from './Comment.css';
+
+interface UserProfile {
+  name: string;
+  src: string;
+  alt: string;
+}
+interface CommentProps {
+  user: UserProfile;
+  comment: string;
+}
+
+export const Comment = ({ user, comment }: CommentProps) => {
+  return (
+    <Flex direction="column" align="flexStart" width="1064px" gap="1.2rem">
+      <Flex direction="row" align="center" gap="0.8rem" justify="center">
+        <Profile src={user.src} alt={user.alt} />
+        <Text variant="sm_caption_medium" color="grayscale90">
+          {user.name}
+        </Text>
+      </Flex>
+      <div className={styles.comment}>{comment}</div>
+    </Flex>
+  );
+};
