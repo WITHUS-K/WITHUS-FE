cac88a6dd55a5c3184a15b15d6bd38f4f8a75576
cindy-chaewon <chaewon1769@daum.net>
2025-04-27 19:48:36 +0900

    feat: SearchInput 컴포넌트 (#14)

--- packages/ui/src/icons/src/colored/index.ts
+++ packages/ui/src/icons/src/colored/index.ts
@@ -10,6 +10,7 @@ export { default as IcDelete } from './IcDelete';
 export { default as IcDropdwonGray90 } from './IcDropdwonGray90';
 export { default as IcHeaderLogo } from './IcHeaderLogo';
 export { default as IcHeaderProfile } from './IcHeaderProfile';
+export { default as IcInputDelete } from './IcInputDelete';
 export { default as IcInputError } from './IcInputError';
 export { default as IcInputSearch } from './IcInputSearch';
 export { default as IcInputSuccess } from './IcInputSuccess';
