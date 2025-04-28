a2007a823cfb41d7dfdc80bed7fceca6f65fc0a4
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-04-28 22:54:33 +0900

    feat: 공통 컴포넌트 빌드 (#17)

--- packages/ui/src/components/DropDown/index.ts
+++ packages/ui/src/components/DropDown/index.ts
@@ -1,2 +1,3 @@
 export { default as SelectDropdown } from './SelectDropdown/SelectDropdown';
 export { default as ClubDropdown } from './SelectDropdown/ClubDropdwon';
+export { default as SelectScoreDropdown } from './SelectDropdown/SelectScoreDropdown';
