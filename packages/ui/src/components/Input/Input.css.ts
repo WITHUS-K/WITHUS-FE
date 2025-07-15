b09fd5b5d191964f45586145b977a92841766929
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-07-16 00:35:53 +0900

    feat: QuestionInput focus 스타일 추가 (#76)

--- packages/ui/src/components/Input/Input.css.ts
+++ packages/ui/src/components/Input/Input.css.ts
@@ -135,9 +135,13 @@ export const commentInputWrapper = style({
     '&[has-error="true"]': {
       border: `1px solid ${vars.colors.error}`,
     },
+    '&:focus-within': {
+      border: `1px solid ${vars.colors.primary50}`,
+    },
   },
 });
 
+  
 export const questionTitleInput = style({
   all: 'unset',
   width: '100%',
