3d227523d8c19286b414c21ff051fa76b6b9c032
cindy-chaewon <chaewon1769@daum.net>
2025-07-17 00:42:02 +0900

    fix: 쿠키 옵션 도메인 수정 (#84)

--- apps/web/src/api/authCookies.ts
+++ apps/web/src/api/authCookies.ts
@@ -6,7 +6,7 @@ export const cookieOptions = {
   path: '/',
   sameSite: 'lax' as const,
   secure: isProd,
-  ...(isProd
-    ? { domain: '.recruit-withus.co.kr' } // 진짜 프로덕션 도메인일 때만
-    : {}),
+  /* ...(isProd
+    ? { domain: 'www.recruit-withus.co.kr' } // ← 여기를 www로
+    : {}),*/
 };
