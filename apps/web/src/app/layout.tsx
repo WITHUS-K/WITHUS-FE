9c3405c10fed5e1cd89bec207fac641d04ebbed4
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-07-16 12:04:18 +0900

    feat: 클라리티 연동 (#76)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,11 +3,14 @@ import '@repo/theme/themes';
 import { themeClass } from '@repo/theme';
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
+import Script from 'next/script';
 import './globals.css';
 import { Providers } from '@web/components/providers/Providers';
+
 const pretendard = localFont({
   src: '../../public/font/subset-PretendardVariable.woff2',
 });
+
 export const metadata: Metadata = {
   title: 'WithUS',
   description: '큐시즘 밋업프로젝트 7팀',
@@ -21,6 +24,18 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${pretendard.className} ${themeClass}`}>
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "semapiq713");`,
+          }}
+        />
+
         <Providers>{children}</Providers>
       </body>
     </html>
