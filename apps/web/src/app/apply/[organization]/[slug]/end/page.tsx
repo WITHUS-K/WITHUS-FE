8faa530777c1c0093c476653f74a9e987f9abf19
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-07-20 18:36:05 +0900

    feat: 지원 마감 페이지 구현 (#119)

--- apps/web/src/app/apply/[organization]/[slug]/end/page.tsx
+++ apps/web/src/app/apply/[organization]/[slug]/end/page.tsx
@@ -1,10 +1,38 @@
 'use client';
 
+import { Divider } from '@repo/ui';
 import { Flex } from '@repo/ui/Flex';
 import { Text } from '@repo/ui/Text';
-import { IcAnnotationAlert } from '@repo/ui/icons/colored';
+import { IcExpiredDateLg, IcExpiredDateSm } from '@repo/ui/icons/colored';
+import { useRecruitmentBySlugQuery } from '@web/store/query/useRecruitmentBySlugQuery';
+import { useParams } from 'next/navigation';
 
 export default function EndPage() {
+  const { slug } = useParams<{ slug: string }>();
+  const { data } = useRecruitmentBySlugQuery({ slug });
+
+  const isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+
+  if (isMobile)
+    return (
+      <Flex
+        direction="column"
+        height="100%"
+        width="100%"
+        align="center"
+        justify="center"
+      >
+        <Flex direction="column" align="center" justify="center" gap="2.4rem">
+          <IcExpiredDateSm width={43.755} height={47.999} />
+          <Text variant="lg_subtitle_bold" color="grayscale80">
+            지원 기간이 아닙니다.
+          </Text>
+        </Flex>
+      </Flex>
+    );
   return (
     <Flex
       direction="column"
@@ -12,10 +40,18 @@ export default function EndPage() {
       width="100%"
       align="center"
       justify="center"
+      gap="8.4rem"
     >
-      <Flex direction="column" align="center" justify="center" gap="2.4rem">
-        <IcAnnotationAlert width={48} height={48} />
-        <Text variant="lg_subtitle_bold" color="grayscale80">
+      <Flex direction="column" align="center" justify="center" gap="2.8rem">
+        <Text variant="xl_title_semibold">
+          [{data.organizationName}]<br />
+          {data.title}
+        </Text>
+        <Divider length="43.4rem" borderColor="grayscale10" />
+      </Flex>
+      <Flex direction="column" align="center" justify="center" gap="3.2rem">
+        <IcExpiredDateLg width={134} height={147} />
+        <Text variant="lg_subtitle_medium" color="grayscale80">
           지원 기간이 아닙니다.
         </Text>
       </Flex>
---
+++ packages/ui/src/icons/src/colored/IcExpiredDateSm.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgIcExpiredDateSm = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={44}
+    height={48}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#BECEFF"
+      d="M.123 17.305h43.755V44.08A3.92 3.92 0 0 1 39.96 48H4.04A3.92 3.92 0 0 1 .123 44.08z"
+    />
+    <path
+      fill="#2C60FF"
+      d="M.123 10.449A4.9 4.9 0 0 1 5.021 5.55h33.96a4.9 4.9 0 0 1 4.897 4.898v6.857H.123z"
+    />
+    <rect
+      width={6.531}
+      height={11.755}
+      x={8.939}
+      fill="#9EB6FF"
+      fillOpacity={0.5}
+      rx={3.265}
+    />
+    <rect
+      width={6.531}
+      height={11.755}
+      x={28.531}
+      fill="#9EB6FF"
+      fillOpacity={0.5}
+      rx={3.265}
+    />
+    <path
+      fill="#fff"
+      d="M21.999 42.447c5.41 0 9.796-4.385 9.796-9.796s-4.386-9.796-9.796-9.796-9.796 4.386-9.796 9.796S16.59 42.447 22 42.447"
+    />
+    <path
+      stroke="#5680FF"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2.449}
+      d="M22 28.734v3.919M22 36.57h.01"
+    />
+  </svg>
+);
+export default SvgIcExpiredDateSm;
